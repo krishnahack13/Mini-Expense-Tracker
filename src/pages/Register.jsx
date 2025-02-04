f6a6ea33c35575978430a0157a7911215e0f00cb
krishnahack13 <130061736+krishnahack13@users.noreply.github.com>
2025-02-04 15:56:17 +0530

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -13,7 +13,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('https://mini-expense-tracker-backend-ddpt.onrender.com/api/auth/register', { firstName, lastName, email, password });
+      const res = await axios.post('${process.env.REACT_APP_BACKEND_URL}/api/auth/register', { firstName, lastName, email, password });
       alert(res.data.message);
       navigate('/login');
     } catch (err) {
