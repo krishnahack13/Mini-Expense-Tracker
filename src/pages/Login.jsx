11d13669b98c866b80f4a5fe101be696e7850920
krishnahack13 <130061736+krishnahack13@users.noreply.github.com>
2025-02-04 15:55:44 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('https://mini-expense-tracker-backend-ddpt.onrender.com/api/auth/login', { email, password }, { withCredentials: true });
+      const res = await axios.post('${process.env.REACT_APP_BACKEND_URL}/api/auth/login', { email, password }, { withCredentials: true });
       alert(res.data.message);
       navigate('/dashboard');
     } catch (err) {
